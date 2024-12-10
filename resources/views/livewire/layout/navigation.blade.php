c70483c77de2fd0d7085a94656146b11eaf81043
Md Abdur Razzaq Masum <masummdar@gmail.com>
2024-12-10 22:23:07 +0600

    adds -- some more routes for testing

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -33,6 +33,9 @@ new class extends Component
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('settings')" :active="request()->routeIs('settings')" wire:navigate>
+                        {{ __('Settings') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::view('/', 'welcome');
 
 Route::view('/products', 'products');
 
+Route::view('/settings', 'settings')
+    ->middleware(['auth', 'verified'])
+    ->name('settings');
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
